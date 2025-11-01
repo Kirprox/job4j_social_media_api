8b92efd9252b41cd3c64969daf6d20032bce217d
kir <kirpro95@yandex.ru>
2025-11-01 12:05:46 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -1,7 +1,16 @@
 package ru.job4j.socialmediaapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.socialmediaapi.model.Post;
+import ru.job4j.socialmediaapi.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUser(User user);
+    List<Post> findByCreatedBetween(LocalDateTime start, LocalDateTime end);
+    Page<Post> findAll(Pageable pageable);
 }
